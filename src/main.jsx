51e418259bb37ee2589c11f650d798ef2755eba9
Shalem Raju <shalemrajubiraja7@gmail.com>
2026-07-20 17:09:30 +0530

    Added forward slash in base name in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode basename="/Java_FullStack_Project_Food_Ordering_System_Front-end">
+  <StrictMode basename="/Java_FullStack_Project_Food_Ordering_System_Front-end/">
     <BrowserRouter>
       <App />
     </BrowserRouter>
